0e5d164d0834912e71d75b8d7ef0b91d5358408d
manlikevee <odahviktor@gmail.com>
2024-03-26 10:59:03 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// When scan is successful fucntion will produce data
+function onScanSuccess(qrCodeMessage) {
+  document.getElementById("result").innerHTML =
+    '<span class="result">' + qrCodeMessage + "</span>";
+    alert('yippee')
+    html5QrCodeScanner.stop();
+    html5QrCodeScanner.clear();
+}
+
+// When scan is unsuccessful fucntion will produce error message
+function onScanError(errorMessage) {
+  // Handle Scan Error
+}
+
+// Setting up Qr Scanner properties
+var html5QrCodeScanner = new Html5QrcodeScanner("reader", {
+  fps: 10,
+  qrbox: 250
+});
+
+// in
+html5QrCodeScanner.render(onScanSuccess, onScanError);
